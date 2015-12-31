52267913c7d224f5a7f6e11549957779a5526202
vittalp <vittalp88@gmail.com>
2015-12-30 17:10:17 -0800

    perform cross entropy with beta 0.9

--- src/caffe/layers/sigmoid_cross_entropy_loss_layer.cu
+++ src/caffe/layers/sigmoid_cross_entropy_loss_layer.cu
@@ -6,6 +6,7 @@
 #include "caffe/util/math_functions.hpp"
 #include "caffe/vision_layers.hpp"
 
+#define BETA 0.9  // hard code beta
 namespace caffe {
 
 template <typename Dtype>
@@ -22,8 +23,8 @@ void SigmoidCrossEntropyLossLayer<Dtype>::Forward_gpu(
   const Dtype* target = bottom[1]->cpu_data();
   Dtype loss = 0;
   for (int i = 0; i < count; ++i) {
-    loss -= input_data[i] * (target[i] - (input_data[i] >= 0)) -
-        log(1 + exp(input_data[i] - 2 * input_data[i] * (input_data[i] >= 0)));
+    loss -= ( target[i] == 0 ? 1-BETA : BETA ) * (input_data[i] * (target[i] - (input_data[i] >= 0)) -
+        log(1 + exp(input_data[i] - 2 * input_data[i] * (input_data[i] >= 0))));
   }
   top[0]->mutable_cpu_data()[0] = loss / num;
 }
@@ -43,10 +44,22 @@ void SigmoidCrossEntropyLossLayer<Dtype>::Backward_gpu(
     const Dtype* sigmoid_output_data = sigmoid_output_->gpu_data();
     const Dtype* target = bottom[1]->gpu_data();
     Dtype* bottom_diff = bottom[0]->mutable_gpu_diff();
-    caffe_copy(count, sigmoid_output_data, bottom_diff);
-    caffe_gpu_axpy(count, Dtype(-1), target, bottom_diff);
+
+    caffe_copy(count, sigmoid_output_data, bottom_diff); 
+    //for (int i = 0; i < count; ++i) {
+      //bottom_diff[i] = target[i]*sigmoid_output_data[i]*0.96940; // pn pnhat (2b-1)
+    //}
+    caffe_gpu_mul<Dtype>(count, target, sigmoid_output_data, bottom_diff);
+    caffe_gpu_scal(count, Dtype(2*BETA-1), bottom_diff);
+    caffe_gpu_axpy(count, Dtype(1-BETA), sigmoid_output_data, bottom_diff);
+    caffe_gpu_axpy(count, Dtype(-BETA), target, bottom_diff);
+    
+    //caffe_copy(count, sigmoid_output_data, bottom_diff); // sigmoid_output is p hat; target is p
+    //caffe_gpu_axpy(count, Dtype(-1), target, bottom_diff);
+
     // Scale down gradient
     const Dtype loss_weight = top[0]->cpu_diff()[0];
+    //LOG(INFO) << "top0-mutablecpudata = " << top[0]->mutable_cpu_data()[0] << " loss_weight = " << loss_weight;
     caffe_gpu_scal(count, loss_weight / num, bottom_diff);
   }
 }
